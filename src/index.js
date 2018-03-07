e3ff9760fed4a4f48a5dfbedb3be79e15fcb19a0
Shu Uesugi <shu@chibicode.com>
2018-03-06 19:51:33 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-export default { a: 1 }
+import qs from 'qs'
+
+const isObject = x => Object.prototype.toString.call(x) === '[object Object]'
+
+/**
+ * Parses query strings using qs library,
+ * but modifies the result so that it correctly parses
+ * array of objects without indices. See tests for an example.
+ *
+ * NOTE: this doesn't work recursively.
+ *
+ * @see https://github.com/ljharb/qs/issues/215
+ * @see https://github.com/ljharb/qs/issues/252
+ * @param  {string} queryString - Query string to parse.
+ * @param  {Object} qsOpts      - Options for qs library.
+ * @return {Object} Parsed object.
+ */
+export const parseQueryString = ({ queryString, qsOpts }) => {
+  const tempResult = qs.parse(queryString, qsOpts)
+  Object.keys(tempResult).forEach(queryKey => {
+    const value = tempResult[queryKey]
+
+    // Is value an array of 1 object where each value is an array?
+    // Example:
+    // [{ a: [1, 2], b: [3, 4] }]
+    if (
+      Array.isArray(value) &&
+      value.length === 1 &&
+      isObject(value[0]) &&
+      Object.keys(value[0]).every(k => Array.isArray(value[0][k]))
+    ) {
+      const newValue = []
+      // If so, turn it into an array of multiple objects
+      // [{ a: [1, 2], b: [3, 4] }] becomes
+      // [{ a: 1, b: 2 }, { a: 3, b: 4 }]
+      Object.keys(value[0]).forEach(key => {
+        value[0][key].forEach((itemInArray, i) => {
+          newValue[i] = newValue[i] || {}
+          newValue[i][key] = itemInArray
+        })
+      })
+      tempResult[queryKey] = newValue
+    } else {
+      tempResult[queryKey] = value
+    }
+  })
+  return tempResult
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,62 @@
 import test from 'ava'
-import sample from '../dist/index'
+import { parseQueryString } from '../dist/index'
 
-test('foo', t => {
-  t.truthy(sample.a === 1)
+test('Parses object queries correctly', t => {
+  const queryString = ['key[a]=1', 'key[b]=2', 'key[c]=3', 'key[d]=4'].join('&')
+  t.deepEqual(parseQueryString({ queryString }), {
+    key: {
+      a: '1',
+      b: '2',
+      c: '3',
+      d: '4'
+    }
+  })
+})
+
+test('Parses array of 1 object correctly', t => {
+  const queryString = [
+    'key[][a]=1',
+    'key[][b]=2',
+    'key[][c]=3',
+    'key[][d]=4'
+  ].join('&')
+  t.deepEqual(parseQueryString({ queryString }), {
+    key: [
+      {
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4'
+      }
+    ]
+  })
+})
+
+test('Parses array of 2 objects correctly', t => {
+  const queryString = [
+    'key[][a]=1',
+    'key[][b]=2',
+    'key[][c]=3',
+    'key[][d]=4',
+    'key[][a]=5',
+    'key[][b]=6',
+    'key[][c]=7',
+    'key[][d]=8'
+  ].join('&')
+  t.deepEqual(parseQueryString({ queryString }), {
+    key: [
+      {
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4'
+      },
+      {
+        a: '5',
+        b: '6',
+        c: '7',
+        d: '8'
+      }
+    ]
+  })
 })
